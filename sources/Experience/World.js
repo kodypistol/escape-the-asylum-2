d941ef3fe0489ef83bd719271120536e4a8345f6
Jules Mesnil <jules.mesnil@gmail.com>
2024-10-17 15:14:58 +0200

    snap global vertices & affine textures

--- sources/Experience/World.js
+++ sources/Experience/World.js
@@ -26,22 +26,44 @@ export default class World
     }
 
     vertexSnapping() {
-        // Define the resolution
-        const resolution = new THREE.Vector2(320, 240);
+      // Define the resolution
+      const resolution = new THREE.Vector2(320, 240);
 
-        // Replace existing THREE vertex shader to apply snapping to all objects across the scene
-        THREE.ShaderChunk.project_vertex = THREE.ShaderChunk.project_vertex.replace(
-            'gl_Position = projectionMatrix * mvPosition;',
-            `
-            // PS1 Vertex Snapping
-            vec4 pos = projectionMatrix * mvPosition;
-            pos.xyz /= pos.w;
-            pos.xy = floor(vec2(${resolution.toArray()}) * pos.xy) / vec2(${resolution.toArray()});
-            pos.xyz *= pos.w;
-            gl_Position = pos;
-            `
-        );
-    }
+      // Replace existing THREE vertex shader to apply snapping to all objects across the scene
+      THREE.ShaderChunk.project_vertex = THREE.ShaderChunk.project_vertex.replace(
+          'gl_Position = projectionMatrix * mvPosition;',
+          `
+          // PS1 Vertex Snapping
+          vec4 pos = projectionMatrix * mvPosition;
+          float dist = length(mvPosition);
+          float affine = dist + (mvPosition.w * 8.0) / dist * 0.5;
+          vAffine = affine;
+          pos.xyz /= pos.w;
+          pos.xy = floor(vec2(${resolution.toArray()}) * pos.xy) / vec2(${resolution.toArray()});
+          pos.xyz *= pos.w;
+          gl_Position = pos;
+          `
+      );
+
+      // Modify the default fragment shader for affine texture mapping
+      THREE.ShaderChunk.uv_pars_vertex = `
+          varying vec2 vUv;
+          varying float vAffine;
+      ` + THREE.ShaderChunk.uv_pars_vertex;
+
+      THREE.ShaderChunk.uv_pars_fragment = `
+          varying vec2 vUv;
+          varying float vAffine;
+      ` + THREE.ShaderChunk.uv_pars_fragment;
+
+      THREE.ShaderChunk.map_fragment = THREE.ShaderChunk.map_fragment.replace(
+          'vec4 texelColor = texture2D( map, vUv );',
+          `
+          vec2 uv = vUv / vAffine;
+          vec4 texelColor = texture2D( map, uv );
+          `
+      );
+  }
 
   handlePlayerCount(playerId, event) {
     if (event.key === "a" || event.key === "x") {
--- sources/Experience/assets.js
+++ sources/Experience/assets.js
@@ -6,6 +6,10 @@ export default [
       { name: "player1Model", source: "/assets/exports/p1_animated.fbx", type: "fbx" },
       { name: "player2Model", source: "/assets/exports/p2_animated.fbx", type: "fbx" },
       { name: "background", source: "/assets/background.hdr", type: "hdr" },
+      { name: "lennaTexture", source: "/assets/lenna.png", type: "texture" },
+      { name: "pigeonTexture", source: "/assets/pigeon.jpg", type: "texture" },
+      { name: "pigeonModel", source: "/assets/pigeon.glb", type: "glb" },
+      { name: "tvModel", source: "/assets/tv.glb", type: "glb" },
     ],
   },
 ];
